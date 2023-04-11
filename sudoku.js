5f3ed8a6439a68d7c262816df832fda446d4e943
dallinforsyth <dallfor192@gmail.com>
2023-04-11 09:53:32 -0500

    started final functino

--- sudoku.js
+++ sudoku.js
@@ -77,5 +77,17 @@ function selectTile() {
       return;
     }
     this.innerText = numSelected.id;
+
+    // "0-0" '0-1" ... "0-3"
+    let coords = this.id.split("-"); //["0", "0"]
+    let r = parseInt(coords[0]);
+    let e = parseInt(coords[1]);
+
+    if (solution[r][e] === numSelected.id) {
+      this.innerText = numSelected.id;
+    } else {
+      errors += 1;
+      document.getElementById("errors").innerText = errors;
+    }
   }
 }
